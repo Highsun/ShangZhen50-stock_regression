1c3d661d94124c7ce151bc26744e8177d25fcc47
Highsun <1339687616@qq.com>
2024-12-12 14:05:29 +0800

    RS

---
+++ code/RMBExchangeMedianPrice.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+if not os.path.exists('../output/display'):
+    os.makedirs('../output/display')
+
+# plt config
+plt.rcParams['font.sans-serif'] = ['Kaiti SC']
+plt.rcParams['axes.unicode_minus'] = False
+
+## Load the data
+df = pd.read_csv('../dataset/人民币汇率中间价_2024.csv', index_col='日期')
+## print(df.info())
+
+## Convert the exchange rates for indirect quotation currencies
+indirect_currencies = [
+    '澳元', '林吉特', '卢布', '兰特', '韩元', 
+    '迪拉姆', '里亚尔', '福林', '波兰兹罗提', 
+    '丹麦克朗', '瑞典克朗', '挪威克朗', '里拉', 
+    '比索', '泰铢'
+]
+
+for currency in indirect_currencies:
+    if currency in df.columns:
+        df[currency] = 100 / df[currency]
+
+## Plot the data
+x = df.index.to_numpy()
+plt.figure(figsize=(10, 8))
+for i in df.columns:
+    plt.plot(x, df[i], label=i)
+plt.xticks(ticks=x[::15], labels=df.index[::15], rotation=45)
+plt.title('人民币汇率中间价')
+plt.xlabel('日期')
+plt.ylabel('中间价')
+plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15), ncol=7)
+plt.tight_layout()
+# plt.show()
+plt.savefig('../output/display/人民币汇率中间价.png', dpi=300)
--- code/linearRegression.py
+++ code/linearRegression.py
@@ -12,16 +12,16 @@ plt.rcParams['axes.unicode_minus'] = False
 
 ## Load and preprocess data
 y_df = pd.read_csv('../dataset/上证指数_2024.csv')
-x_df = pd.read_csv('../dataset/恒生指数_2024.csv')
+x_df = pd.read_csv('../dataset/人民币汇率中间价_2024.csv')
 
 y_df['日期'] = pd.to_datetime(y_df['日期'])
 x_df['日期'] = pd.to_datetime(x_df['日期'])
 
 data_df = pd.merge(y_df, x_df, on='日期', how='inner')
-# print(data_df.info())
+print(data_df.info())
 
-y = data_df['收盘_y'].values
-x = data_df['收盘_x'].values
+y = data_df['美元'].values
+x = data_df['收盘'].values
 
 ## linear regression
 x = x.reshape(-1, 1)
@@ -31,14 +31,14 @@ model = LinearRegression()
 model.fit(x, y)
 
 ## visualization
-title = '线性回归：' + data_df['股票名称_x'][0] + '和' + data_df['股票名称_y'][0]
+title = '线性回归：' + '美元价格' + '和' + data_df['股票名称'][0]
 lr_equation = rf'$f(x) = {model.coef_[0][0]:.2f} x + {model.intercept_[0]:.2f}$'
 
 plt.scatter(x, y, color='blue')
 plt.plot(x, model.predict(x), color='red')
 plt.title(title)
-plt.xlabel(data_df['股票名称_x'][0])
-plt.ylabel(data_df['股票名称_y'][0])
+plt.xlabel('美元价格')
+plt.ylabel(data_df['股票名称'][0])
 plt.text(x.min(), y.max(), lr_equation, fontsize=12, color='red', verticalalignment='top', usetex=True)
 # plt.show()
-plt.savefig(f'../output/lr/lr_{data_df["股票名称_x"][0]}_{data_df["股票名称_y"][0]}.png', dpi=300)
+plt.savefig(f'../output/lr/lr_美元价格_{data_df["股票名称"][0]}.png', dpi=300)
